d35f7d8da3784ac3b14ad83c43690fae237fc33c
Benjamin Baron <benjamin.baron@me.com>
2017-11-17 17:49:13 +0000

    Added option to edit the timeline

--- TrackingAdvisor/ISTimeline.swift
+++ TrackingAdvisor/ISTimeline.swift
@@ -8,6 +8,84 @@
 
 import UIKit
 
+struct Moveable {
+    let layer:CALayer?
+    let label:UILabel?
+    var type = ""
+    var position = -1
+    var maxPosition = -1
+    
+    init(layer:CALayer? = nil, label:UILabel? = nil, type:String = "", position:Int = -1, maxPosition: Int = -1) {
+        self.layer = layer
+        self.label = label
+        self.type = type
+        self.position = position
+        self.maxPosition = maxPosition
+    }
+    
+    init(layer:CALayer, type:String = "", position:Int = -1, maxPosition: Int = -1) {
+        self.init(layer: layer, label: nil, type:type, position: position, maxPosition: maxPosition)
+    }
+    
+    init(label:UILabel, type:String = "", position:Int = -1, maxPosition: Int = -1) {
+        self.init(layer: nil, label: label, type:type, position:position, maxPosition: maxPosition)
+    }
+    
+    func hide() {
+        if let layer = layer {
+            layer.opacity = 0
+        } else if let label = label {
+            label.alpha = 0
+        }
+    }
+    
+    func show() {
+        if let layer = layer {
+            layer.opacity = 1
+        } else if let label = label {
+            label.alpha = 1
+        }
+    }
+    
+    func move(to: CGPoint, with delay: CFTimeInterval) {
+        if let layer = layer {
+            if position > 0 {
+                let animation = CABasicAnimation(keyPath: "position")
+                animation.fromValue = layer.position
+                animation.toValue = to
+                animation.duration = delay
+                layer.position = to
+                layer.add(animation, forKey: nil)
+            }
+        } else if let label = label {
+            if position > 0 {
+                UIView.animate(withDuration: delay) {
+                    label.center = to
+                }
+            }
+        }
+    }
+    
+    func moveDown(by value: CGFloat, with delay:CFTimeInterval) {
+        var position = CGPoint()
+        if let layer = layer {
+            position.x = layer.position.x
+            position.y = layer.position.y + value
+            if type == "line" && self.position == self.maxPosition {
+                print("before: \(position.y) \(value)")
+                position.y += value/CGFloat(self.position)
+                print("after: \(position.y) \(value)")
+            }
+        } else if let label = label {
+            position.x = label.center.x
+            position.y = label.center.y + value
+//            label.center = position
+        }
+        move(to: position, with: delay)
+    }
+}
+
+
 open class ISTimeline: UIScrollView {
     
     fileprivate static let gap:CGFloat = 15.0
@@ -49,6 +127,9 @@ open class ISTimeline: UIScrollView {
         }
     }
     
+    var layers:[Int:[Moveable]] = [:]
+    var addButtons:[Moveable] = []
+    
     open var points:[ISPoint] = [] {
         didSet {
             self.layer.sublayers?.forEach({ (layer:CALayer) in
@@ -71,7 +152,6 @@ open class ISTimeline: UIScrollView {
     }
     
     open var bubbleArrows:Bool = false
-    
     open var timelineTitle:String! { didSet {
             timelineTitleLabel.text = timelineTitle
         }
@@ -79,6 +159,8 @@ open class ISTimeline: UIScrollView {
     
     fileprivate let timelineTitleOffset:CGFloat = 90.0
     fileprivate var timelineTitleLabel:UILabel!
+    fileprivate var testIcon:CALayer!
+    fileprivate let screenSize:CGRect = UIScreen.main.bounds
     
     fileprivate var sections:[(point:CGPoint, bubbleRect:CGRect, descriptionRect:CGRect?, titleLabel:UILabel, descriptionLabel:UILabel?, pointColor:CGColor, lineColor:CGColor, fill:Bool, icon:UIImage, iconBg:CGColor, iconCenter:CGPoint)] = []
     
@@ -98,6 +180,29 @@ open class ISTimeline: UIScrollView {
         buildTimelineTitleLabel()
     }
     
+    @objc fileprivate func editTimeline() {
+        print("Edit timeline button pressed")
+        let duration = 0.5
+        CATransaction.begin()
+        CATransaction.setAnimationDuration(duration)
+        CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut))
+        
+        for i in 1..<self.layers.count {
+            for j in 0..<self.layers[i]!.count {
+                let layer = self.layers[i]![j]
+                layer.moveDown(by: CGFloat(i)*75, with: duration)
+            }
+        }
+        CATransaction.commit()
+        
+        for i in 0..<self.addButtons.count {
+            let layer = self.addButtons[i]
+            layer.show()
+        }
+        
+        self.contentSize = CGSize(width: self.contentSize.width, height: self.contentSize.height + CGFloat((self.layers.count-1))*75)
+    }
+    
     override open func draw(_ rect: CGRect) {
         let ctx:CGContext = UIGraphicsGetCurrentContext()!
         ctx.saveGState()
@@ -106,31 +211,80 @@ open class ISTimeline: UIScrollView {
         timelineTitleLabel.frame = CGRect(x: 1.0, y: 29.0, width: rect.width-1.0, height: 40)
         self.addSubview(timelineTitleLabel)
         
+        // Place the timeline edit button
+        let timelineEditButton = UIButton(type: UIButtonType.system)
+        timelineEditButton.frame.size = CGSize(width: 50, height: 25)
+        timelineEditButton.frame.origin = CGPoint(x: screenSize.width-(timelineEditButton.frame.width+30), y: 43)
+        timelineEditButton.setTitle("Edit", for: .normal)
+        timelineEditButton.addTarget(self, action: #selector(ISTimeline.editTimeline), for: .touchUpInside)
+        self.addSubview(timelineEditButton)
+        
+        testIcon = drawIcon(CGPoint(x: screenSize.width/2 - iconDiameter/2, y: 43), fill: Constants.primaryLight.cgColor, image: UIImage(named: "location")!)
+        self.layer.addSublayer(testIcon)
+        
         for i in 0 ..< sections.count {
+            layers[i] = []
             if (i < sections.count - 1) {
                 var start = sections[i].point
                 start.x += pointDiameter / 2
                 start.y += pointDiameter / 2
                 
                 var end = sections[i + 1].point
-                end.y += pointDiameter / 2
                 end.x = start.x
+                if (i < sections.count - 2) {
+                    end.y = sections[i + 2].point.y
+                }
+                end.y += pointDiameter / 2
                 
                 var cap = 1
                 if i == sections.count - 2 {
                     cap = 2
                 }
-                drawLine(start, end: end, color: sections[i].lineColor, cap: cap)
+                let lineLayer = drawLine(start, end: end, color: sections[i].lineColor, cap: cap)
+                self.layer.addSublayer(lineLayer)
+                layers[i]!.append(Moveable(layer: lineLayer, type: "line", position: i, maxPosition: sections.count - 2))
+                
+                // Add button (with opacity = 0)
+                let addIconPosition = CGPoint(x: sections[i].point.x - lineWidth/2,
+                                              y: sections[i+1].point.y + CGFloat(i) * 75)
+                let addIconLayer = drawIcon(addIconPosition, fill: UIColor.gray.cgColor, image: UIImage(named: "add")!)
+                addIconLayer.opacity = 0
+                addButtons.append(Moveable(layer: addIconLayer, type: "addIcon", position: i, maxPosition: sections.count - 2))
+                self.layer.addSublayer(addIconLayer)
+                
+                // Add text (button)
+                let addTextRect = CGRect(
+                    x: addIconPosition.x + 35,
+                    y: addIconPosition.y + 2,
+                    width: 100,
+                    height: 25)
+                let addTextLabel = buildDescriptionLabel(text: "Add an event")
+                
+                let addTextLayer = drawDescription(addTextRect, textColor: Constants.descriptionColor, descriptionLabel: addTextLabel!)
+                addTextLayer.alpha = 0
+                addButtons.append(Moveable(label: addTextLayer, type: "addText", position: i, maxPosition: sections.count - 2))
+                self.addSubview(addTextLayer)
             }
-            drawIcon(sections[i].iconCenter, fill: sections[i].iconBg, image: sections[i].icon)
-            drawPoint(sections[i].point, color: sections[i].pointColor, fill: sections[i].fill)
-            drawBubble(sections[i].bubbleRect, backgroundColor: Constants.primaryLight, textColor: Constants.titleColor, titleLabel: sections[i].titleLabel)
+            
+            let iconLayer = drawIcon(sections[i].iconCenter, fill: sections[i].iconBg, image: sections[i].icon)
+            self.layer.addSublayer(iconLayer)
+            layers[i]!.append(Moveable(layer: iconLayer, type: "icon", position: i, maxPosition: sections.count - 1))
+            
+            let pointLayer = drawPoint(sections[i].point, color: sections[i].pointColor, fill: sections[i].fill)
+            self.layer.addSublayer(pointLayer)
+            layers[i]!.append(Moveable(layer: pointLayer, type: "point", position: i, maxPosition: sections.count - 1))
+            
+            let bubbleLayer = drawBubble(sections[i].bubbleRect, backgroundColor: Constants.primaryLight, textColor: Constants.titleColor, titleLabel: sections[i].titleLabel)
+            self.addSubview(bubbleLayer)
+            layers[i]!.append(Moveable(label: bubbleLayer, type: "bubble", position: i, maxPosition: sections.count - 1))
             
             print("#\(i): \(sections[i].point)")
             
             let descriptionLabel = sections[i].descriptionLabel
             if (descriptionLabel != nil) {
-                drawDescription(sections[i].descriptionRect!, textColor: Constants.descriptionColor, descriptionLabel: sections[i].descriptionLabel!)
+                let descriptionLayer = drawDescription(sections[i].descriptionRect!, textColor: Constants.descriptionColor, descriptionLabel: sections[i].descriptionLabel!)
+                self.addSubview(descriptionLayer)
+                layers[i]!.append(Moveable(label: descriptionLayer, type: "description", position: i, maxPosition: sections.count - 1))
             }
         }
         
@@ -216,6 +370,10 @@ open class ISTimeline: UIScrollView {
     
     fileprivate func buildDescriptionLabel(_ index:Int) -> UILabel? {
         let text = points[index].description
+        return buildDescriptionLabel(text: text)
+    }
+    
+    fileprivate func buildDescriptionLabel(text: String?) -> UILabel? {
         if (text != nil) {
             let descriptionLabel = UILabel()
             descriptionLabel.text = text
@@ -232,7 +390,7 @@ open class ISTimeline: UIScrollView {
         return self.bounds.width - (self.contentInset.left + self.contentInset.right) - pointDiameter - lineWidth - ISTimeline.gap * 1.5
     }
     
-    fileprivate func drawLine(_ start:CGPoint, end:CGPoint, color:CGColor, cap:Int) {
+    fileprivate func drawLine(_ start:CGPoint, end:CGPoint, color:CGColor, cap:Int) -> CALayer {
         let path = UIBezierPath()
         path.move(to: start)
         path.addLine(to: end)
@@ -241,8 +399,6 @@ open class ISTimeline: UIScrollView {
         shapeLayer.path = path.cgPath
         shapeLayer.strokeColor = color
         shapeLayer.lineWidth = lineWidth
-        
-        self.layer.addSublayer(shapeLayer)
         
         if cap > 0 {
             var roundedCap:UIBezierPath?
@@ -255,11 +411,13 @@ open class ISTimeline: UIScrollView {
             roundedCapLayer.path = roundedCap?.cgPath
             roundedCapLayer.fillColor = color
             roundedCapLayer.lineWidth = 0
-            self.layer.addSublayer(roundedCapLayer)
+            shapeLayer.addSublayer(roundedCapLayer)
         }
+        
+        return shapeLayer
     }
     
-    fileprivate func drawPoint(_ point:CGPoint, color:CGColor, fill:Bool) {
+    fileprivate func drawPoint(_ point:CGPoint, color:CGColor, fill:Bool) -> CALayer {
         let path = UIBezierPath(ovalIn: CGRect(x: point.x, y: point.y, width: pointDiameter, height: pointDiameter))
         
         let shapeLayer = CAShapeLayer()
@@ -268,10 +426,10 @@ open class ISTimeline: UIScrollView {
         shapeLayer.fillColor = fill ? color : UIColor.clear.cgColor
         shapeLayer.lineWidth = 0
         
-        self.layer.addSublayer(shapeLayer)
+        return shapeLayer
     }
     
-    fileprivate func drawIcon(_ point: CGPoint, fill: CGColor, image: UIImage) {
+    fileprivate func drawIcon(_ point: CGPoint, fill: CGColor, image: UIImage) -> CALayer {
         let path = UIBezierPath(ovalIn: CGRect(x: point.x, y: point.y, width: iconDiameter, height: iconDiameter))
         
         let shapeLayer = CAShapeLayer()
@@ -285,22 +443,24 @@ open class ISTimeline: UIScrollView {
         imageLayer.position = CGPoint(x: point.x + iconDiameter/2 ,y: point.y + iconDiameter/2)
         imageLayer.contents = image.cgImage
         
-        self.layer.addSublayer(shapeLayer)
-        self.layer.addSublayer(imageLayer)
+        shapeLayer.addSublayer(imageLayer)
+        return shapeLayer
     }
     
-    fileprivate func drawBubble(_ rect:CGRect, backgroundColor:UIColor, textColor:UIColor, titleLabel:UILabel) {
+    fileprivate func drawBubble(_ rect:CGRect, backgroundColor:UIColor, textColor:UIColor, titleLabel:UILabel) -> UILabel {
         
         let titleRect = CGRect(x: rect.origin.x, y: rect.origin.y, width: rect.size.width - 15, height: rect.size.height - 1)
         titleLabel.textColor = Constants.black
         titleLabel.frame = titleRect
-        self.addSubview(titleLabel)
+        
+        return titleLabel
     }
     
-    fileprivate func drawDescription(_ rect:CGRect, textColor:UIColor, descriptionLabel:UILabel) {
+    fileprivate func drawDescription(_ rect:CGRect, textColor:UIColor, descriptionLabel:UILabel) -> UILabel {
         descriptionLabel.textColor = textColor
         descriptionLabel.frame = CGRect(x: rect.origin.x, y: rect.origin.y, width: rect.width - 10, height: rect.height)
-        self.addSubview(descriptionLabel)
+        
+        return descriptionLabel
     }
     
     override open func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
